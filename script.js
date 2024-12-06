c9a38d6154eb8f8c4941f5042643cbb1a57a8706
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-12-06 16:24:19 -0600

    Pseudocode for adding and removing book to library

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create Array to store books
+// Create a function to construct a book with given information
+// Create a function to add a new book into the library array
+// Create a function that displays each book on the shelf, using a loop
+// Add a button for adding a new book, input results into the book function and call the add a new book to library function
+// Create a function to remove a specific book from the library
+// Add a button to delete or remove the book from the library
+// Add a button for each book to change the read status
