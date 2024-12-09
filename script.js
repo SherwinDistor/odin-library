459233f3158e6eb215c1a0f0f0c34979a298ec5f
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2024-12-09 16:01:06 -0600

    Create edit and remove buttons, fix read and not read logic

--- script.js
+++ script.js
@@ -16,24 +16,30 @@ function addBookToLibrary(event) {
     const title = document.querySelector('#book-title').value;
     const author = document.querySelector('#book-author').value;
     const pages = document.querySelector('#page-number').value;
-    let isRead = document.querySelector('#book-read').value;
+    let isRead;
     
-    if (isRead === 'on') {
+    if (document.querySelector('#book-read').checked) {
         isRead = true;
     } else {
         isRead = false;
     }
+    
 
     const newBook = new Book(title, author, pages, isRead);
     
     myLibrary.push(newBook);
 
+    closePopup(popup);
+    
     displayBooks();
+
+    clearInput();
+
 }
 
 // Create a function that displays each book on the shelf, using a loop
 function displayBooks() {
-    
+
     myLibrary.forEach((book) => {
         
         // Get reference fo the main div
@@ -48,25 +54,45 @@ function displayBooks() {
         // Append child new div to main div
         mainDiv.appendChild(bookDiv);
         
-
+        // Book details and Button div creation
         let bookDetailsDiv = document.createElement("div");
-        bookDetailsDiv.classList.add("book-details")
+        bookDetailsDiv.classList.add("book-details");
         bookDiv.appendChild(bookDetailsDiv);
 
-        // Work on the book-edit-button creation
-
-
+        let bookEditBtn = document.createElement('div');
+        bookEditBtn.classList.add('book-edit-btn');
+        bookDiv.appendChild(bookEditBtn);
+        
+        // Book details creation
         let titleEl = document.createElement('h2');
         titleEl.textContent = book.title;
-        bookDetailsDiv.appendChild(titleEl)
-
+        bookDetailsDiv.appendChild(titleEl);
+        
         let authorEl = document.createElement('h3');
         authorEl.textContent = book.author;
         bookDetailsDiv.appendChild(authorEl);
-
+        
         let pagesEl = document.createElement('p');
         pagesEl.textContent = `Pages: ${book.pages}`;
         bookDetailsDiv.appendChild(pagesEl);
+
+        let isReadEl = document.createElement('div');
+        isReadEl.classList.add('isRead');
+        isReadEl.textContent = book.isRead === true ? 'Read' : 'Not Read';
+        bookDetailsDiv.appendChild(isReadEl);
+
+        console.log(book.isRead);
+
+        // button creation
+        let editBtn = document.createElement('button');
+        editBtn.id = "edit-btn";
+        editBtn.textContent = 'Edit';
+        bookEditBtn.appendChild(editBtn);
+    
+        let removeBtn = document.createElement('button');
+        removeBtn.id = "remove-btn";
+        removeBtn.textContent = 'Remove';
+        bookEditBtn.appendChild(removeBtn);
     })
 }
 
@@ -105,11 +131,23 @@ function closePopup(popup) {
     overlay.classList.remove('active');
 }
 
-// 
-
+// Form submit button defined
 const submitBtn = document.getElementById('submit-btn');
 submitBtn.addEventListener('click', addBookToLibrary)
+
+// Clear input fields function
+function clearInput() {
+    document.querySelector('#book-title').value = '';
+    document.querySelector('#book-author').value = '';
+    document.querySelector('#page-number').value = '';
+}
 
 // Create a function to remove a specific book from the library
 // Add a button to delete or remove the book from the library
 // Add a button for each book to change the read status
+
+
+// Clear display
+function clearDisplay() {
+    
+}
